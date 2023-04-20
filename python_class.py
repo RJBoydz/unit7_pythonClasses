c8b175ce7990079158355feb1db44e7fea15cd50
Your Name <you@example.com>
2023-04-20 09:01:25 -0400

    first commit

---
+++ python_class.py
@@ -0,0 +1,11 @@
+class Fastest_Most_Famous_Cars_In_The_World:
+    def __init__(self, speed, famousness, look):
+       self.speed = speed
+       self.famousness = famousness
+       self.look = look
+       
+Best1 = Fastest_Most_Famous_Cars_In_The_World('Twin mill: looks cool with two engines and goes 231 mph', 'Camaro: Was in the Transformers movies and goes 190 mph', 'Bugatti Chiron Supersport 300: is currently the fastest car in the world it goes 304 mph')
+
+print(Best1.speed)
+print(Best1.famousness)
+print(Best1.look)
