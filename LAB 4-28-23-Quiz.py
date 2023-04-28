ae114b6bc8dd917dd665ceede093753ead0b702d
RJBoydz <130468015+RJBoydz@users.noreply.github.com>
2023-04-28 09:25:27 -0400

    Add files via upload

---
+++ LAB 4-28-23-Quiz.py
@@ -0,0 +1,17 @@
+ #1. Python is used my making and sending code.
+ #2. 
+ #3.The Python dictionary is used to know which python code does what.
+ #4.Github it used to show other people your code, using code in a group of people for a project, and 
+ #5.Python class explains all the feature of python for beginner using python, python dictionary explains what every python code does for people who doesn't know what a code does.
+ #6. you = 'youtube', goo = 'google', bing = 'bing', out = 'outlook', git = 'github', print(you,goo,bing,out,git)
+ #7. 
+ 
+ 
+#10. when python reads a code and there a problem the code will have an error, or the error may causes the code to come out different, lastly if a piece of the code is different from the rest the code will be error and who be the code you will wants 
+#11. 
+
+from zmq import HELLO_MSG
+
+
+def function(name):
+    print(HELLO_MSG, name. How are you todyay?) 
